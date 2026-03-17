d4e41cf4ef592da948158f8234e19cbe2f843061
Andrei Aldea <7153954+Grippy98@users.noreply.github.com>
2026-03-17 15:19:54 -0500

    Reinsert custom 128-bit ServiceUUID to LEAdvertisement1 to support native WebBLE Service discovery

--- scripts/badgebeam_bleserver.py
+++ scripts/badgebeam_bleserver.py
@@ -151,6 +151,7 @@ class Advertisement(dbus.service.Object):
         return {
             'org.bluez.LEAdvertisement1': {
                 'Type': self.ad_type,
+                'ServiceUUIDs': dbus.Array(self.service_uuids, signature='s'),
                 'LocalName': dbus.String(self.local_name),
                 'Discoverable': dbus.Boolean(True)
             }
